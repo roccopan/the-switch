84fcf5242d65e892ef46e7fbe5b995c77d00e9bb
juneate <roccopan@gmail.com>
2015-06-03 16:31:57 -0400

    partial solution: working switch

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
+// The switch is on, so turn it off!
+$(document).on('click', 'button.on', function(e) {
 
+	console.log('turn off!');
+	$(this).removeClass('on').addClass('off');
 
+	// Now what?
+});
+
+// The switch is off, so turn it on! 
+$(document).on('click', 'button.off', function(e) {
+
+	console.log('turn on!');
+	$(this).removeClass('off').addClass('on');
+
+	// Now what?
+});
