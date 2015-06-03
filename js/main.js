5eee1b795af9baf89a35082b02324bbc45fb9765
juneate <roccopan@gmail.com>
2015-06-03 19:13:01 -0400

    solved the lighting issue

--- js/main.js
+++ js/main.js
@@ -2,16 +2,28 @@
 $(document).on('click', 'button.on', function(e) {
 
 	console.log('turn off!');
+
+	// turn the light OFF using a class
 	$(this).removeClass('on').addClass('off');
 
-	// Now what?
+	// Change the test status
+	$('.status').text('Who turned off the lights?!');
+
+	// Now change the background-color
+	$('body').removeClass('light').addClass('dark');
 });
 
 // The switch is off, so turn it on! 
 $(document).on('click', 'button.off', function(e) {
 
 	console.log('turn on!');
+
+	// Turn the light ON using a class
 	$(this).removeClass('off').addClass('on');
 
-	// Now what?
+	// Change the test status
+	$('.status').text('It\'s so bright in here!');
+
+	// Now change the background-color
+	$('body').removeClass('dark').addClass('light');
 });
