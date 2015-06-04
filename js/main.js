0e585504e51804304892723dc3eca28fd186063d
juneate <roccopan@gmail.com>
2015-06-03 20:08:05 -0400

    solved with if statements

--- js/main.js
+++ js/main.js
@@ -1,30 +1,23 @@
-// The switch is on, so turn it off!
-$(document).on('click', 'button.on', function(e) {
+$('button').on('click', function () {
 
-	console.log('turn off!');
+	if ( $(this).hasClass('on') ) {
+		// turn the light OFF using a class
+		$(this).removeClass('on').addClass('off');
 
-	// turn the light OFF using a class
-	$(this).removeClass('on').addClass('off');
+		// Change the test status
+		$('.status').text('Who turned off the lights?!');
 
-	// Change the test status
-	$('.status').text('Who turned off the lights?!');
+		// Now change the background-color
+		$('body').removeClass('light').addClass('dark');
+	}
+	else if ( $(this).hasClass('off') ) {
+		// Turn the light ON using a class
+		$(this).removeClass('off').addClass('on');
 
-	// Now change the background-color
-	$('body').removeClass('light').addClass('dark');
-});
+		// Change the test status
+		$('.status').text('It\'s so bright in here!');
 
-// The switch is off, so turn it on! 
-$(document).on('click', 'button.off', function(e) {
-
-	console.log('turn on!');
-
-	// Turn the light ON using a class
-	$(this).removeClass('off').addClass('on');
-
-	// Change the test status
-	$('.status').text('It\'s so bright in here!');
-
-	// Now change the background-color
-	$('body').removeClass('dark').addClass('light');
-
+		// Now change the background-color
+		$('body').removeClass('dark').addClass('light');
+	}
 });
